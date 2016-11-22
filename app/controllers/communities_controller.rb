14691f0bd454c06f686c4631ff523228d1976363
Emre Surmeli <s.emre.s.8@gmail.com>
2016-11-22 17:23:08 -0500

    add community

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,12 +1,12 @@
 class CommunitiesController < ApplicationController
-  before_action :doorkeeper_authorize!
-
-  def show
-    @community = Communities.all
-  end
-
+  # before_action :doorkeeper_authorize!
   def index
-    @community = Communities.all
+    @current_user = User.find(session[:user_id])
+    @my_community = Communities.where( zipcode: @current_user.zipcode )
+    if @my_community == []
+      @my_community = Communities.create( zipcode: @current_user.zipcode )
+    end
+    render json: @my_community
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :doorkeeper_authorize!
+  # before_action :doorkeeper_authorize!
 
   def new
     @user = User.new
